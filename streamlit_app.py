e600b596b7dbb1d13177af06c58488396c388a58
dipascoal88 <diegopsjp@gmail.com>
2024-09-18 10:44:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Odômetros e horímetros dos VLTs")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
